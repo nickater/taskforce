a28d39588df044e64f179d151ca904091875e907
nick ater <nickolas.ater@gmail.com>
2021-03-19 22:18:20 -0400

    reorganized server file

--- server/server.ts
+++ server/server.ts
@@ -2,17 +2,17 @@ import * as dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import { db } from "./db";
-
-const app = express();
-
-app.use(express.json());
+import { authenticateJWT } from "./services/auth/authorization";
 const PORT = process.env.TASKFORCE_SERVER_PORT || 8000;
+const app = express();
+app.use(express.json());
+
+app.use("/customers", authenticateJWT, require("./routes/customer.route"));
+app.use("/projects", authenticateJWT, require("./routes/project.route"));
+app.use("/tasks", authenticateJWT, require("./routes/task.route"));
+app.use("/taskLogs", authenticateJWT, require("./routes/taskLog.route"));
+app.use("/auth", require("./routes/user.route"));
 
 db.authenticate().then(() => {
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 });
-
-app.use("/customers", require("./routes/customer.route"));
-app.use("/projects", require("./routes/project.route"));
-app.use("/tasks", require("./routes/task.route"));
-app.use("/taskLogs", require("./routes/taskLog.route"));
