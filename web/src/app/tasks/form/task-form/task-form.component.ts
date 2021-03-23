b4cf61f9cd5dea34c4ddbbf50fb58ea3717175f9
nick ater <nickolas.ater@gmail.com>
2021-03-22 23:36:30 -0400

    added delete task

--- web/src/app/tasks/form/task-form/task-form.component.ts
+++ web/src/app/tasks/form/task-form/task-form.component.ts
@@ -1,15 +1,110 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { of } from 'rxjs';
+import { switchMap, map, take } from 'rxjs/operators';
+import ITask from '../../../../../../shared/interfaces/task';
+import { TaskService } from '../../task.service';
 
 @Component({
   selector: 'app-task-form',
   templateUrl: './task-form.component.html',
-  styleUrls: ['./task-form.component.scss']
+  styleUrls: ['./task-form.component.scss'],
 })
 export class TaskFormComponent implements OnInit {
+  taskFormGroup: FormGroup;
+  customerId: string;
+  projectId: string;
+  taskId: string;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private fb: FormBuilder,
+    private taskService: TaskService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.initializeForm();
   }
 
+  initializeForm() {
+    this.isNewOrEdit()
+      .pipe(
+        switchMap((res) => {
+          if (res) {
+            return this.taskService.getTaskById(res);
+          } else {
+            return of(undefined);
+          }
+        })
+      )
+      .subscribe((task) => {
+        this.taskFormGroup = this.buildForm(task);
+      });
+  }
+
+  isNewOrEdit() {
+    const options = {
+      customerId: 1,
+      projectId: 3,
+      new: 5,
+      edit: {
+        option: 6,
+        taskId: 5,
+      },
+    };
+
+    return this.route.url.pipe(
+      map((urlSegs) => {
+        this.customerId = urlSegs[options.customerId].path;
+        this.projectId = urlSegs[options.projectId].path;
+
+        if (urlSegs[options.new].path === 'new') {
+          return undefined;
+        } else if (
+          urlSegs[options.edit.option].path === 'edit' &&
+          urlSegs[options.edit.taskId]?.path
+        ) {
+          this.taskId = urlSegs[options.edit.taskId].path;
+          return urlSegs[options.edit.taskId].path;
+        }
+      })
+    );
+  }
+
+  buildForm(task?: ITask) {
+    const formGroup = this.fb.group({
+      description: this.fb.control('', Validators.required),
+      projectId: this.fb.control(this.projectId),
+      isActive: this.fb.control(true),
+    });
+    if (task) {
+      formGroup.get('description').patchValue(task.description);
+    }
+    return formGroup;
+  }
+
+  submit() {
+    of(this.taskId)
+      .pipe(
+        switchMap((id) => {
+          if (id) {
+            console.log(this.taskFormGroup.value);
+            return this.taskService.updateTask({
+              id,
+              ...this.taskFormGroup.value,
+            });
+          } else {
+            return this.taskService.createTask(this.taskFormGroup.value);
+          }
+        }),
+        take(1)
+      )
+      .subscribe(() =>
+        this.router.navigate([
+          `/customers/${this.customerId}/projects/${this.projectId}/tasks`,
+        ])
+      );
+  }
 }
--- web/src/app/tasks/view/task-view/task-view.component.ts
+++ web/src/app/tasks/view/task-view/task-view.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { faTrashAlt, faEdit, faPlus } from '@fortawesome/free-solid-svg-icons';
-import { Observable } from 'rxjs';
-import { switchMap, tap } from 'rxjs/operators';
+import { Observable, Subject } from 'rxjs';
+import { switchMap, take, tap } from 'rxjs/operators';
 import IProject from '../../../../../../shared/interfaces/project';
 import { TaskService } from '../../task.service';
 
@@ -17,20 +17,31 @@ export class TaskViewComponent implements OnInit {
   edit = faEdit;
   plus = faPlus;
   //
-  projectAndTasks$: Observable<IProject>;
+  taskAndTaskLogs$: Observable<IProject>;
+  projectId: string;
   constructor(
     private route: ActivatedRoute,
-    private taskService: TaskService
+    private taskService: TaskService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
-    this.projectAndTasks$ = this.route.url.pipe(
-      switchMap((urlSegs) => this.getTasks(urlSegs[3].path)),
-      tap(console.log)
+    this.taskAndTaskLogs$ = this.route.url.pipe(
+      tap((urlSegs) => (this.projectId = urlSegs[3].path)),
+      switchMap((urlSegs) => this.getTasks(urlSegs[3].path))
     );
   }
 
   getTasks(projectId: string) {
     return this.taskService.getTasks(projectId);
+  }
+
+  deleteTask(taskId: number) {
+    if (confirm('Are you sure you want to delete this task?')) {
+      this.taskAndTaskLogs$ = this.taskService.deleteTask(taskId).pipe(
+        take(1),
+        switchMap(() => this.getTasks(this.projectId))
+      );
+    }
   }
 }
