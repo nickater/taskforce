fff240fa49ccd247c83769ec1d47f8b4a246c32c
nick ater <nickolas.ater@gmail.com>
2021-03-24 11:18:53 -0400

    added breadcrumbs

---
+++ web/src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -0,0 +1,60 @@
+import { TitleCasePipe } from '@angular/common';
+import { Component, OnChanges, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { distinctUntilChanged, filter, map, take, tap } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-breadcrumbs',
+  templateUrl: './breadcrumbs.component.html',
+  styleUrls: ['./breadcrumbs.component.scss'],
+})
+export class BreadcrumbsComponent implements OnInit {
+  breadcrumbsList$: Observable<BreadcrumbLink[]>;
+
+  constructor(private router: Router) {}
+  ngOnInit(): void {
+    this.breadcrumbsList$ = this.buildBreadCrumbs();
+  }
+
+  buildBreadCrumbs() {
+    return this.router.events.pipe(
+      filter((event) => event instanceof NavigationEnd),
+      distinctUntilChanged(),
+      map((navEnd: NavigationEnd) => this.buildLinks(navEnd.url))
+    );
+  }
+
+  buildLinks(url: string): BreadcrumbLink[] {
+    let urlPieces = url.split('/');
+    urlPieces.shift();
+    let breadCrumbs: BreadcrumbLink[] = [];
+    let urlAccumulator = 'customers';
+
+    for (let i = 1; i < urlPieces.length; i++) {
+      if (i % 2 !== 0) {
+        let url = `${urlAccumulator}`;
+        breadCrumbs.push({
+          url,
+          title: this.pickTitle(urlPieces[i - 1]),
+        });
+        urlAccumulator = `${urlAccumulator}/${urlPieces[i]}/${
+          urlPieces[i + 1]
+        }`;
+      }
+    }
+
+    return breadCrumbs;
+  }
+
+  pickTitle(url: string): string {
+    if (!url) return 'Customers';
+    const titleCase = new TitleCasePipe();
+    return titleCase.transform(url);
+  }
+}
+
+export interface BreadcrumbLink {
+  title: string;
+  url: string;
+}
