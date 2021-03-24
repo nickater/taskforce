9100379cb494bbb8ce6d288623c6cbe17252dfe2
nick ater <nickolas.ater@gmail.com>
2021-03-24 11:19:34 -0400

    implemented route guard for all routes

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './authentication/auth.guard';
 import { LoginComponent } from './authentication/login/login.component';
 import { RegisterComponent } from './authentication/register/register.component';
 import { CustomerFormComponent } from './customers/customer-form/customer-form.component';
@@ -23,46 +24,61 @@ const routes: Routes = [
   {
     path: 'customers',
     component: CustomerViewComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'customers/new',
     component: CustomerFormComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'customers/:id/edit',
     component: CustomerFormComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'customers/:id/projects',
     component: ProjectViewComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'customers/:id/projects/new',
     component: ProjectFormComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'customers/:id/projects/:id/edit',
     component: ProjectFormComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'customers/:id/projects/:id/tasks',
     component: TaskViewComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'customers/:id/projects/:id/tasks/new',
     component: TaskFormComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'customers/:id/projects/:id/tasks/:id/edit',
     component: TaskFormComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'customers/:id/projects/:id/tasks/:id/logs',
     component: TaskLogViewComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'customers/:id/projects/:id/tasks/:id/logs/new',
     component: TaskLogFormComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: '**',
+    redirectTo: '/customers',
   },
 ];
 
