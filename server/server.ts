816e761fc456b04e8f24cd200b05bb03a7505536
nick ater <nickolas.ater@gmail.com>
2021-03-28 12:18:54 -0400

    redirected middlewar call to respective controllers

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,16 @@
-import * as dotenv from "dotenv";
+import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
 import cors from "cors";
 import db from "./db";
 import { authenticateJWT } from "./services/auth/authorization";
+import CustomerRouter from "./routes/customer.route";
+import ProjectRouter from "./routes/project.route";
+import TaskRouter from "./routes/task.route";
+import TaskLogRouter from "./routes/taskLog.route";
+import AuthRouter from "./routes/auth.route";
+
 const PORT = process.env.TASKFORCE_SERVER_PORT || 8000;
 const app = express();
 
@@ -12,11 +18,11 @@ app.use(express.json());
 app.use(cors({ origin: "http://localhost:4200" }));
 
 // Routes
-app.use("/customers", authenticateJWT, require("./routes/customer.route"));
-app.use("/projects", authenticateJWT, require("./routes/project.route"));
-app.use("/tasks", authenticateJWT, require("./routes/task.route"));
-app.use("/taskLogs", authenticateJWT, require("./routes/taskLog.route"));
-app.use("/auth", require("./routes/user.route"));
+app.use("/auth", AuthRouter);
+app.use("/customers", authenticateJWT, CustomerRouter);
+app.use("/projects", authenticateJWT, ProjectRouter);
+app.use("/tasks", authenticateJWT, TaskRouter);
+app.use("/tasklogs", authenticateJWT, TaskLogRouter);
 
 db.authenticate().then(() => {
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
