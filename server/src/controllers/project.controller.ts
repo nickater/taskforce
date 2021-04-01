df3a9b98b5289affce415bb70c9d76b8cf299c4b
nick ater <nickolas.ater@gmail.com>
2021-03-31 20:12:08 -0400

    error cleanup

--- server/src/controllers/project.controller.ts
+++ server/src/controllers/project.controller.ts
@@ -52,28 +52,34 @@ const createProject = async (req: Request, res: Response) => {
     const project = await Project.create(req.body);
     res.json(project);
   } catch (error) {
-    res.status(500).json({
-      error,
-    });
+    res.status(500).json(error);
   }
 };
 
 const updateProject = async (req: Request, res: Response) => {
-  const project = await Project.update(req.body, {
-    where: {
-      id: req.params.id,
-    },
-  });
-  res.json(project);
+  try {
+    const project = await Project.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.json(project);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 };
 
 const deleteProject = async (req: Request, res: Response) => {
-  const project = await Project.destroy({
-    where: {
-      id: req.params.id,
-    },
-  });
-  res.sendStatus(200);
+  try {
+    const project = await Project.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.sendStatus(200);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 };
 
 export {
